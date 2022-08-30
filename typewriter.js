5abe6c6c4bce5de52cc9406a714cbc48468a27bc
emma673r <90177139+emma673r@users.noreply.github.com>
2022-08-30 15:25:26 +0200

    works

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,47 @@
 "use strict";
+
+// all let
+let char;
+let string;
+let maxChar;
+
+// all const
+const h1 = document.getElementById("typewriter");
+
+window.addEventListener("DOMContentLoaded", start);
+
+// start function
+function start() {
+  string = h1.textContent.toString();
+  console.log(string);
+  h1.textContent = "";
+  maxChar = string.length;
+
+  char = maxChar;
+  console.log(maxChar);
+  console.log(char);
+
+  loop();
+}
+
+function loop() {
+  console.log(`loop`);
+
+  if (char <= maxChar && char >= 1) {
+    console.log(string);
+    console.log(char);
+    console.log(maxChar);
+    console.log(string[char]);
+
+    let typewritten;
+    typewritten = string[maxChar - char];
+    h1.textContent += typewritten;
+    h1.classList.add("typewritten");
+
+    char--;
+
+    setTimeout(loop, 250);
+  } else {
+    start();
+  }
+}
