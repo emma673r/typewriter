9d91ba113dbed22a11eec6b00270598d24e357ba
emma673r <90177139+emma673r@users.noreply.github.com>
2022-08-30 16:00:33 +0200

    am implementing sound

--- typewriter.js
+++ typewriter.js
@@ -8,10 +8,19 @@ let maxChar;
 // all const
 const h1 = document.getElementById("typewriter");
 
+const musicBtn = document.getElementById("soundBtn");
+
+const soundKey1 = document.getElementById("typekey1");
+const soundKey2 = document.getElementById("typekey2");
+const soundSpace = document.getElementById("typespace");
+const soundLast = document.getElementById("typeLast");
+const soundReturn = document.getElementById("typereturn");
+
 window.addEventListener("DOMContentLoaded", start);
 
 // start function
 function start() {
+  musicBtn.removeEventListener("click", musicOn);
   string = h1.textContent.toString();
   console.log(string);
   h1.textContent = "";
@@ -20,6 +29,8 @@ function start() {
   char = maxChar;
   console.log(maxChar);
   console.log(char);
+
+  musicBtn.addEventListener("click", musicOn);
 
   loop();
 }
@@ -42,19 +53,53 @@ function loop() {
 
     // setTimeout(loop, 250);
 
+    if (typewritten === "e" || typewritten === "s" || typewritten === "t") {
+      console.log(`1`);
 
-      if (typewritten === "e", "n", "s", "t") {
-        console.log(`1`);
-        
-        setTimeout(loop, 150);
-      } else if (typewritten == "i", "h", "x") {
-        console.log(`2`);
-        setTimeout(loop, 250);
-      } else {
-        console.log(`3`);
-        setTimeout(loop, 190);
-      }
+      setTimeout(loop, 150);
+    } else if (typewritten == "i" || typewritten === "h" || typewritten === "o") {
+      console.log(`2`);
+      setTimeout(loop, 250);
+    } else {
+      console.log(`3`);
+      setTimeout(loop, 190);
+    }
+  } else {
+    start();
+  }
+}
 
+function musicOn() {
+  console.log(`musicOn`);
+  musicBtn.removeEventListener("click", musicOn);
+
+  if (char <= maxChar && char >= 1) {
+    console.log(string);
+    console.log(char);
+    console.log(maxChar);
+    console.log(string[char]);
+
+    let typewritten;
+    typewritten = string[maxChar - char];
+    h1.textContent += typewritten;
+    h1.classList.add("typewritten");
+    // !!!!!!!! add sound for clicks and spaces here
+
+    char--;
+
+    // setTimeout(loop, 250);
+
+    if (typewritten === "e" || typewritten === "s" || typewritten === "t") {
+      console.log(`1`);
+
+      setTimeout(musicOn, 150);
+    } else if (typewritten == "i" || typewritten === "h" || typewritten === "o") {
+      console.log(`2`);
+      setTimeout(musicOn, 250);
+    } else {
+      console.log(`3`);
+      setTimeout(musicOn, 190);
+    }
   } else {
     start();
   }
